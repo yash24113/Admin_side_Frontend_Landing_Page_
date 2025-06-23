d7ecd31d0516e64f48a1c113176f3e553e3d7dcf
yash24113 <yashkharva506@gmail.com>
2025-06-23 12:19:04 +0530

    fix: use REACT_APP_BACKEND_API for axios baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_BACKEND_API || "http://localhost:5000",
 });
 
 export default api;
